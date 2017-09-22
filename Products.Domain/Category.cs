d4dc7ab8a1636593c013df1ae7632f5ec179035f
fabian camargo <fabiancv_1025@outlook.com>
2017-09-22 00:15:40 -0500

    Initial project

---
+++ Products.Domain/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace Products.Domain
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(50, ErrorMessage = "The field {0} only can contains {1} characteres length.")]
+        public string Description { get; set; }
+    }
+}
